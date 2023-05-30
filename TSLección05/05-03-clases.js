702a0e46953838bfe70c3ae00789525f1dfc38e8
Valentin Albornoz <valeg486@gmail.com>
2023-05-30 00:29:39 -0300

    clase 8
    commiteamos clase 8 static

--- TSLección05/05-03-clases.js
+++ TSLección05/05-03-clases.js
@@ -1,9 +1,24 @@
 //let persona3 = new Persona("Carla", "Ponce"); esto no se debe hacer: persona is not defined
 
-class Persona { //Clase Padre
+class Persona {
+  //Clase Padre
+
+  static contadorPersonas = 0; //Atributo estático
+  //email = 'Valor default email'; //Atributo no estático
+
+  static get MAX_OBJ(){ //Este método simula una constante
+    return 5;
+  }
+
   constructor(nombre, apellido) {
     this.nombre = nombre;
     this.apellido = apellido;
+    if(Persona.contadorPersonas < Persona.MAX_OBJ){
+      this.idPersona = ++Persona.contadorPersonas;
+    }else{
+      console.log('Se ha superado el máximo de objetos permitidos')
+    }
+    //console.log("Se incrementa el contador: " + Persona.contadorObjetosPersona);
   }
 
   get nombre() {
@@ -18,34 +33,87 @@ class Persona { //Clase Padre
   set apellido(apellido) {
     this._apellido = apellido;
   }
+  nombreCompleto() {
+    return this.idPersona+" "+ this._nombre + " " + this._apellido;
+  }
+  // Sobreescribiendo el método de la clase padre (Object)
+  toString() {
+    // Regresa un String
+    // Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecución
+    return this.nombreCompleto();
+  }
+
+  static saludar() {
+    console.log("Saludos desde este método static");
+  }
+  static saludar2(persona) {
+    console.log(persona.nombre + " " + persona.apellido);
+  }
 }
 
-class Empleado extends Persona { // Clase hija
-    constructor(nombre, apellido, departamento){
-        super(nombre, apellido);
-        this._departamento = departamento;
-    }
+class Empleado extends Persona {
+  // Clase hija
+  constructor(nombre, apellido, departamento) {
+    super(nombre, apellido);
+    this._departamento = departamento;
+  }
 
-    get departamento(){
-        return this._departamento;
-    }
-    set departamento(departamento){
-        this._departamento = departamento;
-    }
+  get departamento() {
+    return this._departamento;
+  }
+  set departamento(departamento) {
+    this._departamento = departamento;
+  }
+  // Sobreescritura
+  nombreCompleto() {
+    return super.nombreCompleto() + ", " + this._departamento;
+  }
 }
 let persona1 = new Persona("Martín", "Perez");
 console.log(persona1.nombre);
-console.log(persona1.apellido);
 persona1.nombre = "Juan Carlos";
 console.log(persona1.nombre);
 //console.log(persona1);
 let persona2 = new Persona("Carlos", "Lara");
 console.log(persona2.nombre);
-console.log(persona2.apellido);
 persona2.nombre = "María Laura";
-console.log(persona2.nombre);
-//console.log(persona2);
+//console.log(persona2.nombre);
 
-let empleado1 = new Empleado('María', 'Gimenez', 'Sistemas');
+let empleado1 = new Empleado("María", "Gimenez", "Sistemas");
 console.log(empleado1);
-console.log(empleado1.nombre);
+console.log(empleado1.nombreCompleto());
+
+// Object.prototype.toString Esta es la manera de acceder a atributos y métodos de manera dinamica
+console.log(empleado1.toString());
+console.log(persona1.toString());
+
+//persona1.saludar(); no se utilzia desde el objeto
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+//console.log(Persona.email); No puede acceder desde la clase
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(persona1.toString());
+console.log(Persona.contadorPersonas);
+let persona3 = new Persona("Carla", "Pertosi");
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);
+
+console.log(Persona.MAX_OBJ);
+//Persona.MAX_OBJ = 10; //No se puede modificar, ni alterar
+console.log(Persona.MAX_OBJ);
+
+let persona4 = new Persona("Franco", "Diaz");
+console.log(persona4.toString());
+let persona5 = new Persona("Liliana", "Paz");
+console.log(persona5.toString());
